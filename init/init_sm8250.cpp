fd59286533db23fc4b710b38160553951a4c3f26
rounak1619 <rounakmehata9@gmail.com>
2021-09-05 04:41:26 +0000

    sm8250-common:Nuke Build FP *Already handled by source
    Signed-off-by: rounak1619 <rounakmehata9@gmail.com>

--- init/init_sm8250.cpp
+++ init/init_sm8250.cpp
@@ -50,12 +50,6 @@ void property_override(char const prop[], char const value[]) {
     __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_multi(char const system_prop[], char const vendor_prop[],char const bootimage_prop[], char const value[]) {
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-    property_override(bootimage_prop, value);
-}
-
 void load_dalvikvm_properties() {
   struct sysinfo sys;
   sysinfo(&sys);
@@ -223,7 +217,4 @@ void vendor_load_properties() {
 
   // dalvikvm props
   load_dalvikvm_properties();
-
-  // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
 }
